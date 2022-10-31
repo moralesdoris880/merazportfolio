6af327dabe22b3021029bd347268a9dd8e0e65ae
Doris Morales <moralesdoris880@gmail.com>
2022-10-30 22:24:14 -0400

    Tailwind css added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 function App() {
   return (
     <div>
-      <nav>
-        
-      </nav>
-      <p>Home Page</p>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
