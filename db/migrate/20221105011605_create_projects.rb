038094bcbd9e57c80d61c6be2ce93edb16c25c8f
Doris Morales <moralesdoris880@gmail.com>
2022-11-04 21:36:43 -0400

    Migration files added

--- db/migrate/20221105011605_create_projects.rb
+++ db/migrate/20221105011605_create_projects.rb
@@ -1,7 +1,13 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-
+      t.string :title
+      t.string :description
+      t.string :photo_url1
+      t.string :photo_url2
+      t.string :photo_url3
+      t.string :photo_url4
+      t.string :video_url
       t.timestamps
     end
   end
